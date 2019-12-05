473fee589f30adebc05ae7ff41a751f1082d9a3c
Nathan Richter <nrichter@fb.com>
2019-12-05 09:34:33 -0800

    use dictionaries to create navigating directions

--- dictionaries_sets/join.py
+++ dictionaries_sets/join.py
@@ -1,14 +1,3 @@
-myList = ["a", "b", "c", "d"]
-
-newString = ''
-# for c in myList:
-#     newString += c + ", "
-
-newString = ", ".join(myList)
-
-print(newString)
-
-
 locations = {0: "You are sitting in front of a computer learning Python",
              1: "You are standing at the end of a road before a small brick building",
              2: "You are at the top of a hill",
@@ -16,3 +5,42 @@ locations = {0: "You are sitting in front of a computer learning Python",
              4: "You are in a valley beside a stream",
              5: "You are in the forest"}
 
+exits = {0: {"Q": 0},
+         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         2: {"N": 5, "Q": 0},
+         3: {"W": 1, "Q": 0},
+         4: {"N": 1, "W": 2, "Q": 0},
+         5: {"W": 2, "S": 1, "Q": 0}}
+
+vocabulary = {"NORTH": "N",
+              "SOUTH": "S",
+              "EAST": "E",
+              "WEST": "W",
+              "QUIT": "Q"}
+
+# print(locations[0].split())
+# print(locations[3].split(","))
+# print(" ".join(locations[0].split()))
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + availableExits + " ").upper()
+    if len(direction) > 1:
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:
+                direction = vocabulary[word]
+                break
+
+    print()
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
