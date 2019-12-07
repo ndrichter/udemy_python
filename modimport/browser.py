c55f536a70caf984754852bbcdb95372d6ea9974
Nathan Richter <nrichter@fb.com>
2019-12-07 14:50:32 -0800

    turtle and webbrowser modules

---
+++ modimport/browser.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+# webbrowser.open("https://www.python.org")
+
+# able to docs about webbrowser
+# https://docs.python.org/3.6/library/webbrowser
+help(webbrowser)
+
+webbrowser.get(using="safari").open("https://www.python.org")
---
+++ modimport/turtledemo.py
@@ -0,0 +1,15 @@
+import turtle
+
+# turtle.forward(150)
+# turtle.right(250)
+# turtle.forward(150)
+# turtle.circle(50)
+# turtle.done()
+
+# __function__
+# private to module should not be changed
+
+for module in dir(__builtins__):
+    print(module)
+
+help(turtle)
