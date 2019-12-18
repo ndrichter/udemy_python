12797c8e0609251bc8cc4a3471127663e17b8107
ndrichter <ndrichter@gmail.com>
2019-12-18 09:49:43 -0500

    tkinter sandbox

--- modimport/tkinter_demo.py
+++ modimport/tkinter_demo.py
@@ -11,4 +11,18 @@ print(tkinter.TclVersion)
 mainWindow = tkinter.Tk()
 mainWindow.title("Hello World")
 mainWindow.geometry("640x480+50+400")
+
+label = tkinter.Label(mainWindow, text="Hello World")
+label.pack(side='top')
+
+canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
+canvas.pack(side='left')
+
+button1 = tkinter.Button(mainWindow, text='button1')
+button2 = tkinter.Button(mainWindow, text='button2')
+button3 = tkinter.Button(mainWindow, text='button3')
+button1.pack(side='left')
+button2.pack(side='left')
+button3.pack(side='left')
+
 mainWindow.mainloop()
