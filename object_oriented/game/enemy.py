2e0a7a09d7d3ca7e352577d1761dc1a392fe5521
ndrichter <ndrichter@gmail.com>
2019-12-30 09:22:31 -0500

    calling super, overriding methods

--- object_oriented/game/enemy.py
+++ object_oriented/game/enemy.py
@@ -1,21 +1,62 @@
+import random
+
+
 class Enemy:
 
     def __init__(self, name="Enemy", hit_points=0, lives=1):
-        self.name = name
-        self.hit_points = hit_points
-        self.lives = lives
+        self._name = name
+        self._hit_points = hit_points
+        self._lives = lives
+        self._alive = True
 
     def take_damage(self, damage):
-        remaining_points = self.hit_points - damage
+        remaining_points = self._hit_points - damage
         if remaining_points >= 0:
-            self.hit_points = remaining_points
-            print("I took {} points damage and have {} left".format(damage, self.hit_points))
+            self._hit_points = remaining_points
+            print("I took {} points damage and have {} left".format(damage, self._hit_points))
         else:
-            self.lives -= 1
+            self._lives -= 1
+            if self._lives > 0:
+                print("{0._name} lost a life".format(self))
+            else:
+                print("{0._name} is dead".format(self))
+                self._alive = False
 
     def __str__(self):
-        return "Name: {0.name}, Lives: {0.lives}, Hit Points: {0.hit_points}".format(self)
+        return "Name: {0._name}, Lives: {0._lives}, Hit Points: {0._hit_points}".format(self)
 
 
 class Troll(Enemy):
-    pass
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=1, hit_points=23)
+
+    def grunt(self):
+        print("Me {0._name}. {0._name} stomp you.".format(self))
+
+
+class Vampire(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
+
+    def dodges(self):
+        if random.randint(1, 3) == 3:
+            print("{0._name} dodges ******".format(self))
+            return True
+        else:
+            return False
+
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+
+
+class VampireKing(Vampire):
+
+    def __init__(self, name):
+        super().__init__(name=name)
+        self._hit_points = 140
+
+    def take_damage(self, damage):
+        super().take_damage(damage // 4)
--- object_oriented/game/main.py
+++ object_oriented/game/main.py
@@ -1,13 +1,30 @@
 from player import Player
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampire, VampireKing
 
 nate = Player("Nate")
 
-ugly_troll = Troll()
+ugly_troll = Troll("Pug")
 print("Ugly Troll - {}".format(ugly_troll))
 
-another_troll = Troll("Ug", 18, 1)
+another_troll = Troll("Ug")
 print("Another Troll - {}".format(another_troll))
 
-brother = Troll("Urg", 23)
+brother = Troll("Urg")
 print(brother)
+
+ugly_troll.grunt()
+another_troll.grunt()
+brother.grunt()
+
+ugly_troll.take_damage(2)
+
+nasty_vampire = Vampire("Nosferatu")
+print(nasty_vampire)
+
+# while nasty_vampire._alive:
+#     nasty_vampire.take_damage(2)
+#     print(nasty_vampire)
+
+dracula = VampireKing("Dracula")
+print(dracula)
+dracula.take_damage(12)
